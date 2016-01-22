e7ef4ad097a5c582fcf70f21dceb238337751deb
Dominik Egger <dominik@almende.org>
2016-01-22 17:50:06 +0100

    added login menu

--- app/src/main/java/nl/dobots/crownstonehub/MainActivity.java
+++ app/src/main/java/nl/dobots/crownstonehub/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,7 +24,6 @@ import nl.dobots.bluenet.ble.extended.callbacks.IBleDeviceCallback;
 import nl.dobots.bluenet.ble.extended.structs.BleDevice;
 import nl.dobots.bluenet.ble.extended.structs.BleDeviceList;
 import nl.dobots.loopback.CrownstoneRestAPI;
-import nl.dobots.loopback.gui.LoginActivity;
 import nl.dobots.loopback.loopback.UserRepository;
 
 /**
@@ -231,6 +232,30 @@ public class MainActivity extends AppCompatActivity {
 
 	private void onBleDisabled() {
 		_btnScan.setEnabled(false);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		switch(id) {
+			case R.id.action_login: {
+				startActivity(new Intent(this, LoginActivity.class));
+				break;
+			}
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 
 }
